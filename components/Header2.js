bcdba267dd10f5b8c6ba4243a928a8e7043db4e8
pratikpatoliya <pratikpatel05111999@gmail.com>
2022-03-04 17:09:21 +0530

    UI and new screen added

---
+++ components/Header2.js
@@ -0,0 +1,42 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+import colors from '../utils/colors';
+import Icon from 'react-native-vector-icons/Ionicons';
+import styles from '../styles/Header';
+
+const Header = (props) => {
+    const { inamel, title, inamer1, inamer2, preasename2, rightname, isBack } = props;
+    return (
+        <View style={styles.views}>
+            <View style={styles.flexrow}>
+                <View style={styles.flexstart}>
+                    {/* left icon back */}
+                    <TouchableOpacity onPress={isBack} style={{ flexDirection: 'row' }}>
+                        <Icon name={inamel} size={23} style={{ color: colors.white }} />
+                        <View style={{ marginLeft: 12 }}>
+                            <Text style={styles.titlefont}>{title}</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.flexend}>
+                    <View style={styles.flexrow}>
+                        {/* Right side Icon */}
+                        <TouchableOpacity style={{ flexDirection: 'row' }}>
+                            <Icon name={inamer1} size={23} style={[styles.iconstyle,{color:colors.yellow}]} />
+                            <Text style={styles.righttxt}>{rightname}</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress={preasename2}>
+                            <View style={{right:15}}>
+                                <Icon name={inamer2} size={23} style={styles.iconstyle2} />
+                            </View>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+        </View>
+    );
+};
+
+export default Header;
+
