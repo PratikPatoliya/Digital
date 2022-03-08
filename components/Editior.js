924e754129b1e23bd0169868c6cbcd90d0ebe3e6
pratikpatoliya <pratikpatel05111999@gmail.com>
2022-03-08 17:20:35 +0530

    text added

--- components/Editior.js
+++ components/Editior.js
@@ -8,6 +8,7 @@ import {
   SafeAreaView,
   FlatList,
   ScrollView,
+  Dimensions,
 } from 'react-native';
 import colors from '../utils/colors';
 import Icon from 'react-native-vector-icons/MaterialIcons';
@@ -15,6 +16,8 @@ import ImagePicker from 'react-native-image-crop-picker';
 import Slider from '@react-native-community/slider';
 import { BitMapColorPicker as ColorPicker } from 'react-native-bitmap-color-picker';
 import styles from '../styles/Editior';
+import {DragTextEditor} from 'react-native-drag-text-editor';
+const WINDOW = Dimensions.get('window');
 
 const Data = [
   { id: 1, text: 'Hubballi-Regular' },
@@ -23,13 +26,18 @@ const Data = [
 ];
 
 const Editior = props => {
-  const { imageSource, setImageSource } = props;
+  const { imageSource, 
+   } = props;
   const [model, setModel] = useState(false);
   const [fontmodel, setFontmodel] = useState(false);
   const [colormodel, setColormodel] = useState(false);
   const [slider, setSlider] = useState(false);
   const [oldcolor, setOldcolor] = useState('black');
   const [sliderValue, setSliderValue] = useState(15);
+  const [arrayTextData, setArrayTextData] = useState([]);
+  const [textID, setTextID] = useState(0);
+  const [textInAction, setTextInAction] = useState(0);
+  const [defaultLabel, setdefaultLabel] = useState('5454');
   function changeColor(colorRgb, resType) {
     resType === 'end' && setOldcolor(colorRgb);
   }
@@ -76,6 +84,56 @@ const Editior = props => {
       </View>
     );
   };
+
+  function addText() {
+    console.log('Hello');
+    setTextID(textID + 1);
+    let DEFS = {
+      defTextID: textID,
+      defTextValue: defaultLabel,
+      defAlign: 'center',
+      defLetterSpacing: 0,
+      defColor: 'green',
+      defLineHeight: 15,
+      defFontSize: 15,
+    };
+    setArrayTextData([...arrayTextData, DEFS]);
+  }
+  const removeText = c => {
+    const filtered = arrayTextData.filter(x => x.defTextID !== c);
+    console.warn('filtered', filtered);
+    setArrayTextData(filtered);
+    setTextID(arrayTextData.length);
+  };
+
+  let ADDED_TEXTS = arrayTextData.map((ID, index) => {
+    return (
+      <DragTextEditor
+        minWidth={100}
+        minHeight={100}
+        w={200}
+        h={200}
+        x={WINDOW.width / 4}
+        y={WINDOW.height / 3}
+        FontColor={ID.defColor}
+        LineHeight={ID.defLineHeight}
+        TextAlign={ID.defAlign}
+        LetterSpacing={ID.defLetterSpacing}
+        FontSize={ID.defFontSize}
+        TopRightAction={() => removeText(ID.defTextID)}
+        centerPress={() => textInAction(ID.defTextID)}
+        isDraggable={true}
+        isResizable={true}
+        onDragStart={() => console.log('-Drag Started')}
+        onDragEnd={() => console.log('- Drag ended')}
+        onDrag={() => console.log('- Dragging...')}
+        onResizeStart={() => console.log('- Resize Started')}
+        onResize={() => console.log('- Resizing...')}
+        onResizeEnd={() => console.log('- Resize Ended')}
+      />
+    );
+  });
+
   return (
     <View>
       {/* image  */}
@@ -181,14 +239,15 @@ const Editior = props => {
           </View>
         </View>
       </Modal>
-
+      {ADDED_TEXTS}
       <View style={styles.mainview}>
         <View style={styles.mainview1}>
-          <TouchableOpacity style={[styles.flex, styles.top]}>
+          <TouchableOpacity style={[styles.flex, styles.top]} onPress={() => addText()}>
             <Text>
               <Icon name="title" size={32} color={colors.black} />
             </Text>
           </TouchableOpacity>
+         
           <TouchableOpacity
             style={[styles.flex, styles.top]}
             onPress={() => setColormodel(true)}>
--- screen/Frame.js
+++ screen/Frame.js
@@ -1,6 +1,13 @@
 /* eslint-disable prettier/prettier */
 import React, {useState, useEffect} from 'react';
-import {Image, View, Text, ScrollView, FlatList, PermissionsAndroid} from 'react-native';
+import {
+  Image,
+  View,
+  Text,
+  ScrollView,
+  FlatList,
+  PermissionsAndroid,
+} from 'react-native';
 import Header2 from '../components/Header2';
 import Eflatlist from '../components/Eflatlist';
 import Editior from '../components/Editior';
@@ -8,20 +15,23 @@ import Share from 'react-native-share';
 import styles from '../styles/Frame';
 import RNFetchBlob from 'rn-fetch-blob';
 
+
 const Frame = ({route, navigation}) => {
   const [imageSource, setImageSource] = useState(null);
   const categoryImage = route.params.img;
 
   useEffect(() => {
-     request_storage_runtime_permission();
+    request_storage_runtime_permission();
   }, []);
   const renderItem = ({item}) => {
     return (
       <View>
         <Image source={{uri: item.img}} style={styles.image} />
-        {imageSource === null ?
-        (<Text> Not Found Img</Text>) : (<Image source={{uri: imageSource}} style={styles.insideimage} />)
-        }
+        {imageSource === null ? (
+          <Text> Not Found Img</Text>
+        ) : (
+          <Image source={{uri: imageSource}} style={styles.insideimage} />
+        )}
       </View>
     );
   };
@@ -38,49 +48,54 @@ const Frame = ({route, navigation}) => {
     }
   };
 
-  const downloadImage =() => {
+  const downloadImage = () => {
     var date = new Date();
-    var image_URL = 'https://reactnativecode.com/wp-content/uploads/2018/02/motorcycle.jpg';
+    var image_URL =
+      'https://reactnativecode.com/wp-content/uploads/2018/02/motorcycle.jpg';
     var ext = getExtention(image_URL);
-    ext = "." + ext[0];
-    const { config, fs } = RNFetchBlob;
-    let PictureDir = fs.dirs.PictureDir
+    ext = '.' + ext[0];
+    const {config, fs} = RNFetchBlob;
+    let PictureDir = fs.dirs.PictureDir;
     let options = {
       fileCache: true,
       addAndroidDownloads: {
         useDownloadManager: true,
         notification: true,
-        path: PictureDir + "/image_" + Math.floor(date.getTime()
-          + date.getSeconds() / 2) + ext,
-        description: 'Image'
-      }
-    }
-    config(options).fetch('GET', image_URL).then((res) => {
-      alert("Image Downloaded Successfully.");
-    });
-  }
-  
-   const getExtention = (filename) => {
-     return (/[.]/.exec(filename)) ? /[^.]+$/.exec(filename) :
-     undefined;
-   }
-  const request_storage_runtime_permission = async() => {
+        path:
+          PictureDir +
+          '/image_' +
+          Math.floor(date.getTime() + date.getSeconds() / 2) +
+          ext,
+        description: 'Image',
+      },
+    };
+    config(options)
+      .fetch('GET', image_URL)
+      .then(res => {
+        alert('Image Downloaded Successfully.');
+      });
+  };
+
+  const getExtention = filename => {
+    return /[.]/.exec(filename) ? /[^.]+$/.exec(filename) : undefined;
+  };
+  const request_storage_runtime_permission = async () => {
     try {
       const granted = await PermissionsAndroid.request(
         PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
         {
-          'title': 'Digital App Storage Permission',
-          'message': 'Digital App needs access to your storage to download Photos.'
-        }
-      )
+          title: 'Digital App Storage Permission',
+          message:
+            'Digital App needs access to your storage to download Photos.',
+        },
+      );
       if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-      }
-      else {
+      } else {
       }
     } catch (err) {
-      console.warn(err)
+      console.warn(err);
     }
-  }
+  };
 
   return (
     <View style={styles.fview}>
